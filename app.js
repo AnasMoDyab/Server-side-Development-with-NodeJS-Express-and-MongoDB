6444af8130cff3b9fdb64291154d17db462ba803
AnasMoDyab <anasdyab89@gmail.com>
2021-02-10 12:49:46 +0100

    Express Session Part 2

--- app.js
+++ app.js
@@ -7,25 +7,24 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
-const Dishes= require('./models/dishes');
+const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/confusion';
 const connect = mongoose.connect(url);
 
-connect.then((db)=> {
-  console.log("Connect correctly to server");
+connect.then((db) => {
+    console.log("Connect correctly to server");
 
-}), (err)=> {
-  console.log(err)
+}), (err) => {
+    console.log(err)
 };
 
 var app = express();
@@ -36,84 +35,65 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 // cookies
 //app.use(cookieParser('12345-67890-09876-54321'));
 
 app.use(session({
-  name: "session-id",
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
+    name: "session-id",
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
 
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 // authentication basic
-function auth(req, res, next){
+function auth (req, res, next) {
   console.log(req.session);
 
-if(!req.session.user){
-  const authHeader= req.headers.authorization;
-  if(!authHeader ){
-    const err = new Error('Your are not authenticated');
-
-    res.setHeader('www-Authenticated',' Basic');
-    err.status= 401;
+  if(!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
     return next(err);
   }
-
-  const auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
-      .toString().split(':');
-
-  const username= auth[0];
-  const password = auth[1];
-
-  if(username ==='admin' && password==='password'){
-      req.session.user = 'admin';
-
-    next();
-  } else {
-    const err = new Error('Your are not authenticated');
-
-    res.setHeader('www-Authenticated',' Basic');
-    err.status= 401;
-    return next(err);
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
   }
 }
- else {
-   if(req.session.user === 'admin'){
-     next();
-   }else {
-     const err = new Error('Your are not authenticated');
-     err.status= 401;
-     return next(err);
-   }
-}
-}
+
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
