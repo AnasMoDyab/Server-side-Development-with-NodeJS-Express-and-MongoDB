14d7e6a761f4c2c0f8d2b3634aa2d0de2a85236f
AnasMoDyab <anasdyab89@gmail.com>
2021-02-11 11:45:37 +0100

    Cors

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const mongoose = require("mongoose");
 const Dishes = require('./models/dishes');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -59,6 +61,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
