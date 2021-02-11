fa984a629eb0566f8b23d74feaf8b736bab19c3a
AnasMoDyab <anasdyab89@gmail.com>
2021-02-11 12:53:08 +0100

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/confusion'
+    'mongoUrl' : 'mongodb://localhost:27017/confusion',
+    'facebook': {
+        clientId:'933317183873986' ,
+        clientSecret: '1770349a93550ec6780473c0b813eaa2'
+    }
 }
